5b79dd03dedce4a70f541bf71a69dc5f9ccb6bed
abrooksnagel <abrooksnagel@Aarons-MacBook-Air.local>
2015-11-13 17:16:42 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function timesTwo(number) {
+  var product = number * 2;
+  return product;
+}
+
+function minusThree(number) {
+  var difference = number - 3;
+  return difference;
+}
+
+function addTen(number) {
+  var sum = number + 10;
+  return sum;
+}
+
+addTen(minusThree(timesTwo(2)));
